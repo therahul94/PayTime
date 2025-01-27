2b5a9fae21c051605987bec3efd286ad4e0029ed
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-01-27 22:22:25 +0530

    init-changes vercel.json

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ connectDB();
 
 app.use('/api/v1', router);
 
+app.use('/', (req, res)=>{
+    res.send("Hey there! backend is running");
+})
+
 // I have entered this '0.0.0.0', so that using IP address also we can be able to get the data from backend
 app.listen(process.env.PORT, '0.0.0.0', ()=>{
     console.log(`Server connected: ${process.env.PORT}`)
